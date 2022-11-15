733b3fe77cff8b37f75daff755d0bdad38a73708
Ashley Cruz <ashleynayellicruz@gmail.com>
2022-11-15 09:41:14 -0600

    next and previous bttn on pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -5,14 +5,29 @@ import "./styles.scss";
 const Pagination = (props) => {
   const { currentPage, onPageChange, pageSize, totalCount } = props;
 
+  const onPrevious = () => {
+    if (currentPage === 1) return;
+    onPageChange(currentPage - 1);
+  }
+
+  const onNext = () => {
+    if (currentPage === lastPage) return;
+    onPageChange(currentPage + 1);
+  }
+
   const paginationRange = Array.from(
     { length: Math.ceil(totalCount / pageSize) },
     (x, i) => i + 1
   );
+
+  const lastPage = paginationRange.length;
   
   return (
     <div className="pagination">
       <ul className="pagination__container">
+        <li className={classnames('bttn',{disabled: currentPage === 1})} onClick={onPrevious}>
+          <span className="arrow left"></span>
+        </li>
         {paginationRange.map((num, i) => {
           const classesLi = classnames({
             selected: currentPage === num,
@@ -23,6 +38,9 @@ const Pagination = (props) => {
             </li>
           );
         })}
+        <li className={classnames('bttn',{disabled: currentPage === lastPage})} onClick={onNext}>
+        <span className="arrow right"></span>
+        </li>
       </ul>
     </div>
   );
