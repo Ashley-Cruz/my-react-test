7c0ad2e5a9ea8a214ec847d01e3cbff207050991
Ashley Cruz <ashleynayellicruz@gmail.com>
2022-11-14 21:55:43 -0600

    welcome component updated

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import './styles.scss';
 
 const Welcome = () => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate("/todo-list");
+  };
+
   return (
-    <div>Welcome</div>
+    <div className="welcome">
+      <h1>Hello there!</h1>
+      <p>Oops... once you start reading you can't stop, now your mission is to complete the TODO list, good luck!</p>
+      <button onClick={onClick}>Click me</button>
+    </div>
   )
 }
 
